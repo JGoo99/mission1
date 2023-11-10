fc91f2716c8ea91f90a9b526ac2c87cf8f636ea0
JGoo99 <zzgg3040@naver.com>
2023-11-10 15:55:55 +0900

    refactor: grouping functiong & var

--- src/main/webapp/jsFile/location.js
+++ src/main/webapp/jsFile/location.js
@@ -1,19 +1,9 @@
 
+// 내 위치정보 (경도, 위도) 반환
 var myLocation = document.getElementById("myLocation");
-
-var searchWifi = document.getElementById("searchWifi");
 
 myLocation.onclick = function () {
     getUserLocation();
-};
-
-searchWifi.onclick = function () {
-    var lon = document.getElementById("lon").value;
-    var lat = document.getElementById("lat").value;
-
-    var url = "/?lon=" + encodeURIComponent(lon) + "&lat=" + encodeURIComponent(lat);
-
-    window.location.href = url;
 };
 
 function success({coords}) {
@@ -23,9 +13,23 @@ function success({coords}) {
     document.getElementById("lon").value = longitude;
     document.getElementById("lat").value = latitude;
 }
+
 function getUserLocation() {
     if (!navigator.geolocation) {
         throw "위치 정보가 지원되지 않습니다.";
     }
     navigator.geolocation.getCurrentPosition(success);
 }
+
+
+// 내 위치정보 검색
+var searchWifi = document.getElementById("searchWifi");
+
+searchWifi.onclick = function () {
+    var lon = document.getElementById("lon").value;
+    var lat = document.getElementById("lat").value;
+
+    var url = "/?lon=" + encodeURIComponent(lon) + "&lat=" + encodeURIComponent(lat);
+
+    window.location.href = url;
+};
