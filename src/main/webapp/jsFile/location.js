d8bfea2a2e45a196277bdd645b2d6649789ca8c5
JGoo99 <zzgg3040@naver.com>
2023-11-12 20:57:10 +0900

    fix: datatype

--- src/main/webapp/jsFile/location.js
+++ src/main/webapp/jsFile/location.js
@@ -1,6 +1,6 @@
 
 // 내 위치정보 (경도, 위도) 반환
-var myLocation = document.getElementById("myLocation");
+const myLocation = document.getElementById("myLocation");
 
 myLocation.onclick = function () {
     getUserLocation();
@@ -10,10 +10,12 @@ function success({coords}) {
     const longitude = coords.longitude; // 경도
     const latitude = coords.latitude;   // 위도
 
+    // 경도, 위도 input 의 value 로 전달
     document.getElementById("lon").value = longitude;
     document.getElementById("lat").value = latitude;
 }
 
+// 위치정보 수락
 function getUserLocation() {
     if (!navigator.geolocation) {
         throw "위치 정보가 지원되지 않습니다.";
@@ -23,13 +25,18 @@ function getUserLocation() {
 
 
 // 내 위치정보 검색
-var searchWifi = document.getElementById("searchWifi");
+const searchWifi = document.getElementById("searchWifi");
 
 searchWifi.onclick = function () {
-    var lon = document.getElementById("lon").value;
-    var lat = document.getElementById("lat").value;
-
-    var url = "/?lon=" + encodeURIComponent(lon) + "&lat=" + encodeURIComponent(lat);
-
-    window.location.href = url;
+    const lon = document.getElementById("lon").value;
+    const lat = document.getElementById("lat").value;
+    
+    if (lon === "" || lat === "") {
+        alert("위치 정보를 입력한 후에 조회해 주세요.")
+    } else {
+        const url = "/?lon=" + encodeURIComponent(lon) + "&lat=" + encodeURIComponent(lat);
+        
+        // url 의 파라미터로 경도, 위도 전송
+        window.location.href = url;
+    }
 };
